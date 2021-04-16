18b4211d05af7f82bfd0df402b702e5a1f93c716
HY-ZhengWei <HY.ZhengWei@qq.com>
2021-04-16 10:11:53 +0800

    1.0.3

--- test/org/hy/common/video/junit/JU_VideoToMP4.java
+++ test/org/hy/common/video/junit/JU_VideoToMP4.java
@@ -20,11 +20,11 @@ public class JU_VideoToMP4
     @Test
     public void toMP4()
     {
-        VideoHelp.$FFMpegHome = "D:\\WorkSpace_SearchDesktop\\hy.common.video\\ffmpeg-4.1-win64-static";
+        VideoHelp.$FFMpegHome = "C:\\WorkSpace\\hy.common.video\\ffmpeg-4.1-win64-static";
         
-        VideoHelp.toMP4("D:\\WorkSpace_SearchDesktop\\hy.common.video\\test\\inputPath\\20180919-WZYB-1920x1080.mpg"
-                       ,"D:\\WorkSpace_SearchDesktop\\hy.common.video\\test\\outputPath" 
-                       ,720 
+        VideoHelp.toMP4("C:\\WorkSpace\\hy.common.video\\test\\inputPath\\4.mp4"
+                       ,"C:\\WorkSpace\\hy.common.video\\test\\outputPath"
+                       ,720
                        ,480);
     }
     
